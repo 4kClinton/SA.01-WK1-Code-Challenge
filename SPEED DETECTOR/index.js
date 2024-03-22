095c3a28e731c6305ac91c325071a5c08018d01f
CLINTON <issnasty2@gmail.com>
2024-03-22 16:34:17 +0300

    initial commit

---
+++ SPEED DETECTOR/index.js
@@ -0,0 +1,13 @@
+let Speed = 50;
+const speedLimit = 70;
+
+if(Speed<70){
+    console.log("Ok");
+}
+else if(Speed==speedLimit + 5){
+    console.log("DEMERIT:1");
+}
+for (let DEMERIT = 0; DEMERIT < Speed++;) {
+      
+
+    }
